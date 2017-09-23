0fcfa25746deab8f23374f12e8d743afd8ffdc75
akartik1996 <a.kartik1996@gmail.com>
2017-09-23 20:07:38 +0530

    Update Program_Counter.v

--- Program_Counter.v
+++ Program_Counter.v
@@ -1,22 +1,17 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    19:13:49 09/23/2017 
-// Design Name: 
-// Module Name:    Program_Counter 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Company: 		3ST Technologies
+// Engineer: 		Kartik Agarwal
+// Create Date:    	19:13:49 09/23/2017 
+// Module Name:    	Program_Counter 
+// Project Name:   	RISC_8-bit
+// Target Devices: 	Xilinx Spartan XC3S100e-5cp132
+// Tool versions:  	Xilinx ISE Design suite 14.7 Webpack version
+// Description: 	Program counter contain the next instruction address to be executed. 
+//			This address will be input the program RAM to access a specific line of instructions. 
+//			Normally, PC would be increased after every instruction executed to 
+//			point to the next address except if flow control instructions is executed which modify the bits contain in the PC.
+
 //////////////////////////////////////////////////////////////////////////////////
 module Program_Counter(
     input clock,
